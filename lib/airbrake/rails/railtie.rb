c769543da6e9c09c8c90a51bbbdaf03a8871181d
EmperDeon <EmperDeon@protonmail.com>
2020-11-12 19:31:11 +0500

    Fixed Rails 6

--- lib/airbrake/rails/railtie.rb
+++ lib/airbrake/rails/railtie.rb
@@ -11,7 +11,7 @@ module Airbrake
         # case the request is local. We want to insert our middleware after
         # DebugExceptions, so we don't notify Airbrake about local requests.
 
-        if ::Rails.version.start_with?('5.')
+        if ::Rails.version.start_with?('5.', '6.')
           # Avoid the warning about deprecated strings.
           # Insert after DebugExceptions, since ConnectionManagement doesn't
           # exist in Rails 5 anymore.
